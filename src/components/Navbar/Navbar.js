7f73d52a437bd20393582813dc69227ec5ebd113
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2021-03-18 19:57:36 +0100

    solved auth issues

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logout from "../Auth/Logout"
-import * as ReactBootStrap from 'react-bootstrap'
-
+import Logout from "../Auth/Logout";
+import * as ReactBootStrap from "react-bootstrap";
 
 export function Navbar() {
   return (
-    <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <ReactBootStrap.Navbar.Brand href="/">Home</ReactBootStrap.Navbar.Brand>
-    <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-      <ReactBootStrap.Nav className="mr-auto">
-        <ReactBootStrap.Nav.Link href="/signup">Signup</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href="/login">Login</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.NavDropdown title="Motocycle Menu" id="collasible-nav-dropdown">
-          <ReactBootStrap.NavDropdown.Item href="/motocycles">List of Motocycles</ReactBootStrap.NavDropdown.Item>
-          <ReactBootStrap.NavDropdown.Item href="/motocycles/:motocycleId">Motocycle detail</ReactBootStrap.NavDropdown.Item>
-          <ReactBootStrap.NavDropdown.Item href="/motocycles/create">Create your Motocycle</ReactBootStrap.NavDropdown.Item>
-          <ReactBootStrap.NavDropdown.Divider />
-          
-        </ReactBootStrap.NavDropdown>
-      </ReactBootStrap.Nav>
-      <ReactBootStrap.Nav>
-        <ReactBootStrap.Nav.Link href="#login">Login</ReactBootStrap.Nav.Link>
-        <Logout/>
-       
-        
-      </ReactBootStrap.Nav>
-    </ReactBootStrap.Navbar.Collapse>
-  </ReactBootStrap.Navbar>
-
-
-
-
-  )
+    <ReactBootStrap.Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <ReactBootStrap.Navbar.Brand href="/">Home</ReactBootStrap.Navbar.Brand>
+      <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto">
+          <ReactBootStrap.Nav.Link>
+            <Link to="/signup">Signup</Link>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link>
+            <Link to="/login">Login</Link>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.NavDropdown
+            title="Motocycle Menu"
+            id="collasible-nav-dropdown"
+          >
+            <ReactBootStrap.NavDropdown.Item>
+              <Link to="/motocycles">List of Motocycles</Link>
+            </ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Item>
+              <Link to="/motocycles/:motocycleId">Motocycle detail</Link>
+            </ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Item>
+              <Link to="/motocycles/create">Create your Motocycle</Link>
+            </ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Divider />
+          </ReactBootStrap.NavDropdown>
+        </ReactBootStrap.Nav>
+        <ReactBootStrap.Nav>
+          <ReactBootStrap.Nav.Link>
+            <Link to="/login"></Link>
+          </ReactBootStrap.Nav.Link>
+          <Logout />
+        </ReactBootStrap.Nav>
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
+  );
 }
 /*
 
@@ -54,4 +66,3 @@ export function Navbar() {
 );
 
 */
-
