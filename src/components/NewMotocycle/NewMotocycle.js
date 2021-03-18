a31253341bfbe97d94721a19a529bd09bdea6258
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2021-03-18 20:13:05 +0100

    solve delete issue

--- src/components/NewMotocycle/NewMotocycle.js
+++ src/components/NewMotocycle/NewMotocycle.js
@@ -1,34 +1,54 @@
 import React from "react";
-import {useMotocycle} from "../../context/motocycleContext"
+import { useMotocycle } from "../../context/motocycleContext";
 
 const NewMotocycle = () => {
-    const initialState = {marca:"", modelo:"", image:""}
-    const[state, setState] = React.useState(initialState)
-    const {createMotocycle} = useMotocycle()
+  const initialState = { marca: "", modelo: "", image: "" };
+  const [state, setState] = React.useState(initialState);
+  const { createMotocycle } = useMotocycle();
 
-    const handleUpload = async (e)=>{
-      console.log("e.target", e.target.files[0])
-     
-      const uploadData = new FormData();
-      uploadData.append('imageUrl', e.target.files[0]);
-      await createMotocycle(state)
-      setState(initialState)
-    }
+  const handleUpload = async (e) => {
+    console.log("e.target", e.target.files[0]);
+
+    const uploadData = new FormData();
+    uploadData.append("imageUrl", e.target.files[0]);
+    await createMotocycle(state);
+    setState(initialState);
+  };
 
   return (
-    <form onSubmit={async (e)=>{
+    <form
+      onSubmit={async (e) => {
         e.preventDefault();
-        await createMotocycle(state)
-        setState(initialState)
-    }}>
+        await createMotocycle(state);
+        setState(initialState);
+      }}
+    >
       <label htmlFor="marca">Marca:</label>
-      <input type="text"  name="marca" value={state.marca} onChange={({target}) => setState({...state,[target.name]: target.value})}/>
+      <input
+        type="text"
+        name="marca"
+        value={state.marca}
+        onChange={({ target }) =>
+          setState({ ...state, [target.name]: target.value })
+        }
+      />
       <label htmlFor="modelo">Modelo:</label>
-      <input type="number"  name="modelo" value={state.modelo} onChange={({target}) =>setState ({...state,[target.name]: target.value})} />
+      <input
+        type="number"
+        name="modelo"
+        value={state.modelo}
+        onChange={({ target }) =>
+          setState({ ...state, [target.name]: target.value })
+        }
+      />
       <label htmlFor="image">Image:</label>
-      <input type="file"  name="image" value={state.image} onChange={handleUpload} />
-      <button type ="submit"> create </button>
-      
+      <input
+        type="file"
+        name="image"
+        value={state.image}
+        onChange={handleUpload}
+      />
+      <button type="submit"> create </button>
     </form>
   );
 };
