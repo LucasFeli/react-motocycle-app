18687a282569a5122e8058e0e616b0e58a6130c6
LucasFeli <felipe298@gmail.com>
2021-03-30 18:47:41 +0200

    icon

--- src/views/PageError/PageError.js
+++ src/views/PageError/PageError.js
@@ -6,7 +6,7 @@ export const PageError = () => {
       <div className="container mt-5">
         <div className="card">
           <div className="card-body">
-            <h3 className="card-title"> Page not found 404 </h3>
+            <h3 className="card-title"> Upps... esta pagina no existe  404 </h3>
           </div>
         </div>
       </div>
